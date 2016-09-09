41363a67a03b13c80d42413831e68113bab63383
vi000246 <vi000246@gmail.com>
2016-09-09 14:59:28 +0800

    test

---
+++ JapaneseAtWork/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JapaneseAtWork
+{
+    public partial class Form1 : Form
+    {
+        //全部片假名、平假名所用的集合佇列
+        public Queue<ItemCollection> queue;
+        public ItemCollection obj;
+        
+        public List<ItemCollection> CollectionList = new List<ItemCollection>();
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "";
+            //將背景設成黃綠 再將黃綠的顏色通通設成透明
+            this.BackColor = Color.YellowGreen; this.TransparencyKey = Color.YellowGreen;
+
+            //增加註解
+            ToolTip toolTip1 = new ToolTip();
+            toolTip1.ShowAlways = true;
+            toolTip1.SetToolTip(this.label3, "輸入完羅馬拼音後請按空白鍵");
+
+            //取得平假名、片假名清單 並轉成佇列
+            var list = Model.HiraganaClean;
+            //打亂排序
+            list.Shuffle();
+            //將list轉成佇列
+            queue = new Queue<ItemCollection>(list);
+            //取出佇列
+            obj = queue.Dequeue();
+            label1.Text = obj.Name;
+
+
+        }
+
+
+        #region 視窗相關
+
+        //關閉代碼
+        private void button1_Click(object sender, EventArgs e)
+        {
+                Application.Exit();
+        }
+
+        //最小化
+        private void pnlFormMin_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        //最大化還原切換
+        //最大化還原切換
+        private bool max = false;
+        private void pnlFormMax_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (max)
+            {
+                this.WindowState = FormWindowState.Normal;
+                max = false;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                max = true;
+            }
+        }
+
+        //窗體拖動
+        private Point mousePoint = new Point();
+        private void pnlCloseBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            this.mousePoint.X = e.X;
+            this.mousePoint.Y = e.Y;
+        }
+
+        private void pnlCloseBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Top = Control.MousePosition.Y - mousePoint.Y;
+                this.Left = Control.MousePosition.X - mousePoint.X;
+            }
+        }
+        #endregion
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //輸入空白鍵
+            if (e.KeyChar == ' ') 
+            { 
+
+                //如果答對 取出下一個物件
+                if (textBox2.Text.ToLower().Trim() == obj.Spell)
+                {
+                    //清掉錯誤提示
+                    label2.Text = "";
+
+                    if (queue.Count > 0)
+                    {
+                        obj = queue.Dequeue();
+                        label1.Text = obj.Name;
+                    }
+                    else
+                    {
+                        label1.Text = "NA";
+                    }
+                }
+                //如果答錯 顯示答案
+                else {
+                    label2.Text = obj.Spell;
+                }
+                //清空TextBox
+                textBox2.Clear();
+            }
+        }
+
+    }
+
+    public class Person
+    {
+        public string Name { set; get; }
+    }
+
+
+}
---
+++ JapaneseAtWork/Model.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JapaneseAtWork
+{
+    public static class Model
+    {
+        /// <summary>
+        /// 平假名 清音
+        /// </summary>
+
+        public static readonly List<ItemCollection> HiraganaClean =
+        new List<ItemCollection>() { 
+            new ItemCollection("あ","a","あ"), 
+            new ItemCollection("い","i","あ"),
+            new ItemCollection("う","u","あ"),
+            new ItemCollection("え","e","あ"),
+            new ItemCollection("お","o","あ"),
+            new ItemCollection("か","ka","か"),
+            new ItemCollection("き","ki","か"),
+            new ItemCollection("く","ku","か"),
+            new ItemCollection("け","ke","か"),
+            new ItemCollection("こ","ko","か"),
+            new ItemCollection("さ","sa","さ"),
+            new ItemCollection("し","shi","さ"),
+            new ItemCollection("す","su","さ"),
+            new ItemCollection("せ","se","さ"),
+            new ItemCollection("そ","so","さ"),
+            new ItemCollection("た","ta","た"),
+            new ItemCollection("ち","chi","た"),
+            new ItemCollection("つ","tsu","た"),
+            new ItemCollection("て","te","た"),
+            new ItemCollection("と","to","た"),
+            new ItemCollection("な","na","な"),
+            new ItemCollection("に","ni","な"),
+            new ItemCollection("ぬ","nu","な"),
+            new ItemCollection("ね","ne","な"),
+            new ItemCollection("の","no","な"),
+            new ItemCollection("は","ha","は"),
+            new ItemCollection("ひ","hi","は"),
+            new ItemCollection("ふ","fu","は"),
+            new ItemCollection("へ","he","は"),
+            new ItemCollection("ほ","ho","は"),
+            new ItemCollection("ま","ma","ま"),
+            new ItemCollection("み","mi","ま"),
+            new ItemCollection("む","mu","ま"),
+            new ItemCollection("め","me","ま"),
+            new ItemCollection("も","mo","ま"),
+            new ItemCollection("や","ya","や"),
+            new ItemCollection("ゆ","yu","や"),
+            new ItemCollection("よ","yo","や"),
+            new ItemCollection("ら","ra","ら"),
+            new ItemCollection("り","ri","ら"),
+            new ItemCollection("る","ru","ら"),
+            new ItemCollection("れ","re","ら"),
+            new ItemCollection("ろ","ro","ら"),
+            new ItemCollection("わ","wa","わ"),
+            new ItemCollection("を","wo","わ"),
+            new ItemCollection("ん","n","ん")
+        };
+
+        /// <summary>
+        /// 片假名 清音
+        /// </summary>
+        public static readonly List<ItemCollection> KatakanaClean =
+            new List<ItemCollection>() { 
+                       new ItemCollection("あ","a","あ"), 
+            new ItemCollection("い","i","あ"),
+            new ItemCollection("う","u","あ"),
+            new ItemCollection("え","e","あ"),
+            new ItemCollection("お","o","あ"),
+            new ItemCollection("か","ka","か"),
+            new ItemCollection("き","ki","か"),
+            new ItemCollection("く","ku","か"),
+            new ItemCollection("け","ke","か"),
+            new ItemCollection("こ","ko","か"),
+            new ItemCollection("さ","sa","さ"),
+            new ItemCollection("し","shi","さ"),
+            new ItemCollection("す","su","さ"),
+            new ItemCollection("せ","se","さ"),
+            new ItemCollection("そ","so","さ"),
+            new ItemCollection("た","ta","た"),
+            new ItemCollection("ち","chi","た"),
+            new ItemCollection("つ","tsu","た"),
+            new ItemCollection("て","te","た"),
+            new ItemCollection("と","to","た"),
+            new ItemCollection("な","na","な"),
+            new ItemCollection("に","ni","な"),
+            new ItemCollection("ぬ","nu","な"),
+            new ItemCollection("ね","ne","な"),
+            new ItemCollection("の","no","な"),
+            new ItemCollection("は","ha","は"),
+            new ItemCollection("ひ","hi","は"),
+            new ItemCollection("ふ","fu","は"),
+            new ItemCollection("へ","he","は"),
+            new ItemCollection("ほ","ho","は"),
+            new ItemCollection("ま","ma","ま"),
+            new ItemCollection("み","mi","ま"),
+            new ItemCollection("む","mu","ま"),
+            new ItemCollection("め","me","ま"),
+            new ItemCollection("も","mo","ま"),
+            new ItemCollection("や","ya","や"),
+            new ItemCollection("ゆ","yu","や"),
+            new ItemCollection("よ","yo","や"),
+            new ItemCollection("ら","ra","ら"),
+            new ItemCollection("り","ri","ら"),
+            new ItemCollection("る","ru","ら"),
+            new ItemCollection("れ","re","ら"),
+            new ItemCollection("ろ","ro","ら"),
+            new ItemCollection("わ","wa","わ"),
+            new ItemCollection("を","wo","わ"),
+            new ItemCollection("ん","n","ん")
+        }; 
+
+        /// <summary>
+        /// 平假名 濁音與半濁音
+        /// </summary>
+        public static readonly List<ItemCollection> HiraganaMuddy =
+            new List<ItemCollection>() { 
+            new ItemCollection("","",""), 
+            new ItemCollection("","","")
+        };
+
+        /// <summary>
+        /// 片假名 濁音與半濁音
+        /// </summary>
+        public static readonly List<ItemCollection> KatakanaMuddy =
+            new List<ItemCollection>() { 
+            new ItemCollection("","",""), 
+            new ItemCollection("","","")
+        };
+
+        /// <summary>
+        /// 平假名 拗音
+        /// </summary>
+        public static readonly List<ItemCollection> HiraganaBend =
+            new List<ItemCollection>() { 
+            new ItemCollection("","",""), 
+            new ItemCollection("","","")
+        };
+
+        /// <summary>
+        /// 片假名 拗音
+        /// </summary>
+        public static readonly List<ItemCollection> KatakanaBend =
+            new List<ItemCollection>() { 
+            new ItemCollection("","",""), 
+            new ItemCollection("","","")
+        };
+
+
+        /// <summary>
+        /// 打亂List的排序
+        /// </summary>
+        private static Random rng = new Random();  
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+
+    public class ItemCollection
+    {
+        //名稱
+        public string Name { get; set; }
+        //拼音
+        public string Spell { get; set; }
+        //位在哪一行
+        public string Row { get; set; }
+
+        public ItemCollection(string Name,string Spell,string Row) {
+            this.Name = Name;
+            this.Spell = Spell;
+            this.Row = Row;
+        }
+
+    }
+}
