94ee995a171b042667f8a8067c4aa1dfdca80e6f
vi000246 <vi000246@gmail.com>
2016-09-10 22:12:42 +0800

    123

--- JapaneseAtWork/Properties/Settings.Designer.cs
+++ JapaneseAtWork/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace JapaneseAtWork.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool HiraganaBend {
             get {
                 return ((bool)(this["HiraganaBend"]));
