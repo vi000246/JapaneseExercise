171f8355a8295217e63d37f095dffb1013b2bb28
vi000246 <vi000246@gmail.com>
2017-01-18 09:24:58 +0800

    123

---
+++ JapaneseAtWork/Form2.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace JapaneseAtWork
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "存檔";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(-1, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "設定出題範圍";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(88, 71);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "平假名";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "清音",
+            "濁音",
+            "拗音"});
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 10);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(76, 55);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkedListBox2);
+            this.groupBox2.Location = new System.Drawing.Point(106, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(85, 71);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "片假名";
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.CheckOnClick = true;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "清音",
+            "濁音",
+            "拗音"});
+            this.checkedListBox2.Location = new System.Drawing.Point(6, 10);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(76, 55);
+            this.checkedListBox2.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 137);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+    }
+}
---
+++ JapaneseAtWork/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JapaneseAtWork
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //依照setting的值 將treeview打勾
+            //平假名 清音
+            this.checkedListBox1.SetItemChecked(0, (bool)Properties.Settings.Default["HiraganaClean"]);
+            //平假名 濁音
+            this.checkedListBox1.SetItemChecked(1, (bool)Properties.Settings.Default["HiraganaMuddy"]);
+            //平假名 拗音
+            this.checkedListBox1.SetItemChecked(2, (bool)Properties.Settings.Default["HiraganaBend"]);
+            //片假名 清音
+            this.checkedListBox2.SetItemChecked(0, (bool)Properties.Settings.Default["KatakanaClean"]);
+            //片假名 濁音
+            this.checkedListBox2.SetItemChecked(1, (bool)Properties.Settings.Default["KatakanaMuddy"]);
+            //片假名 拗音
+            this.checkedListBox2.SetItemChecked(2, (bool)Properties.Settings.Default["KatakanaBend"]);
+
+        }
+
+
+
+        //存檔事件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //將Setting存進app裡
+            //平假名 清音
+            Properties.Settings.Default["HiraganaClean"] = checkedListBox1.GetItemChecked(0);
+            //平假名 濁音
+            Properties.Settings.Default["HiraganaMuddy"] = checkedListBox1.GetItemChecked(1);
+            //平假名 拗音
+            Properties.Settings.Default["HiraganaBend"] = checkedListBox1.GetItemChecked(2);
+            //片假名 清音
+            Properties.Settings.Default["KatakanaClean"] = checkedListBox2.GetItemChecked(0);
+            //片假名 濁音
+            Properties.Settings.Default["KatakanaMuddy"] = checkedListBox2.GetItemChecked(1);
+            //片假名 拗音
+            Properties.Settings.Default["KatakanaBend"] = checkedListBox2.GetItemChecked(2);
+            Properties.Settings.Default.Save(); // Saves settings in application configuration file
+            this.Close();
+        }
+
+    }
+
+
+}
+
